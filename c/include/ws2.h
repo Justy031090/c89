290f37381d5294545338afc915bd21e9e3eefbd3
Michael.Bar <justy031090@gmail.com>
2024-06-09 18:25:17 +0300

    removing dummies + header file of ws2

--- c/include/ws2.h
+++ c/include/ws2.h
@@ -1,15 +1,17 @@
 #ifndef __WS2_H__
 #define __WS2_H__
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 void SwapInt(int *val1, int *val2);
 
-int *CopyArray(int array[], int size);
+void CopyArray(int *source, int *target, int size);
 
 void PrintAddresses();
 
-int SwapwTwoSize_t(size_t *ptr1, size_t *ptr2);
+void SwapTwoSize_t(size_t *ptr1, size_t *ptr2);
 
-int SwapTwoPointers(size_t **ptr1, size_t **ptr2)
+void SwapTwoPointers(size_t **ptr1, size_t **ptr2);
 
 #endif /* __WS2_H__ */
