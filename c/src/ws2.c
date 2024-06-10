724ee741b0bf0d63ac78dc3716d752f9f9cae7f0
Michael.Bar <justy031090@gmail.com>
2024-06-10 09:24:35 +0300

    added functionality for SwapInt, CopyArray and PrintAdress. First printing tests in main.

--- c/src/ws2.c
+++ c/src/ws2.c
@@ -1,1 +1,39 @@
 #include "ws2.h"
+
+
+void SwapInt(int *val1, int *val2)
+{
+	int temp = *val1;
+	*val1 = *val2;
+	*val2 = temp;
+}
+
+void CopyArray(int *source, int *target, int size)
+{
+	int i;
+	
+	for (i = 0; i < size; i++)
+	{
+		target[i] = source[i];
+	}
+}
+
+void PrintAddresses()
+{
+	static int s_i = 7;
+	int i = 7;
+	int *ptr = &i;
+	int *ptr2 = (int *)malloc(sizeof(int));
+	
+	printf("Memory adress of variable s_i is: %p\n", (void *)(&s_i));
+	printf("Memory adress of variable i is: %p\n", (void *)(&i));
+	printf("Memory adress of variable ptr s: %p\n", (void *)(&ptr));
+	printf("Memory adress of variable ptr2: %p\n", (void *)(&ptr2));
+	
+	/*if(ptr)
+	{
+		int **ptr3 = &ptr;
+	}
+	free(ptr2);
+	ptr2 = NULL; */
+}
--- c/test/ws2_test.c
+++ c/test/ws2_test.c
@@ -1,6 +1,25 @@
 #include "ws2.h"
 
 int main()
-{
+{	
+	int *ptr, *ptr2, i = 2, j = 8, index = 0;
+	int arr1[] = {2,3,4,5};
+	int arr2[4];
+	ptr = &i;
+	ptr2 = &j;
+
+	
+	SwapInt(ptr, ptr2);
+	printf("%d\n%d\n\n\n", *ptr, *ptr2);
+
+	CopyArray(arr1, arr2, 4);
+	
+	for (index; index < 4; index++)
+	{
+		printf("%d\n", arr2[index]);
+		
+	}
+
+	PrintAddresses();
 	return 0;
 }
