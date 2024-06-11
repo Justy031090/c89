44fa3bd73bbee7ff9023c5f249873b0425d9993a
Michael.Bar <justy031090@gmail.com>
2024-06-11 13:21:09 +0300

    added functionality to StrnCpy, StrnCmp %%%% Header file updated %%%% test ws3 updated

--- c/src/ws3.c
+++ c/src/ws3.c
@@ -1,1 +1,66 @@
-#include "ws3.h"
+
+#include <stdio.h>
+#include <string.h>
+
+char *StrCpy(char *dst, const char *src)
+{	
+    int len = 0;
+	while (*src != '\0') 
+	{
+	        *dst = *src;
+	        dst++;
+	        src++;
+	        len++;
+	}
+	
+	*dst = '\0';
+	dst -= len;
+	return dst;
+}
+
+char *StrnCpy(char *dst, const char *src, size_t dsize)
+{
+	size_t i;
+	for(i=0; i<dsize; i++)
+	{
+		if (*src != '\0')
+		{
+			*dst = *src;
+			dst++;
+			src++;
+		}
+		else
+		{	
+			*dst = 0;
+			dst++;
+		}
+	
+	}
+	
+	*dst = '\0';
+	dst -= dsize;
+	return dst;
+}
+
+int StrnCmp(const char *str1, const char *str2, size_t n)
+{
+	int i;	
+	for(i=0; i<n; i++)
+	{
+		if(*str1 != *str2)
+		{
+			return (int)*str1 - (int)*str2;
+		}
+		if(*str1 == '\0' ||* str2 == '\0' )
+		{
+			return 0;
+		}
+		str1++;
+		str2++;
+	}
+	
+	return 0;
+
+}
+
+
