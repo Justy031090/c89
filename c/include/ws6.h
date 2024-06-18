d7113f4d529b74119f209681063d83036407d788
Michael.Bar <justy031090@gmail.com>
2024-06-18 16:00:22 +0300

    q

---
+++ c/include/ws6.h
@@ -0,0 +1,11 @@
+#ifndef __WS6_H__
+#define __WS6_H__
+
+
+static table[127];
+static typedef void (*func_ptr)(void);
+void PressKeyIf(void);
+void PressKeySwitch(void);
+void PressKeyLut(void);
+
+#endif /* __WS5_H__ */
