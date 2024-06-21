a5bb3e68ce1171c41f1ea61fec8d80c95249f06d
Michael.Bar <justy031090@gmail.com>
2024-06-21 21:17:16 +0300

    Refactoring after Code Review

--- c/src/ws7.c
+++ c/src/ws7.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-***********************REVIEWD BY *********************************************
+***********************REVIEWD BY TAMIR****************************************
 ******************************************************************************/
 
 #include <stdio.h>             /*Inputs, outpus, printing, writing*/
@@ -12,10 +12,10 @@
 #define PRINT_ARR_SIZE (10)    /*The size of the array for Printing Function*/
 
 
-typedef int (*cmd_func)(FILE *file, char* filename);
-typedef int (*compare_ptr)(const char*, const char*, size_t);
-typedef void (*print_func_type)(int);
-typedef enum LoggerReturn { Error = -1, Exit , Continue, Success} logger_return;
+typedef int (*cmd_func_t)(char* filename);
+typedef int (*compare_ptr_t)(const char*, const char*, size_t);
+typedef void (*print_func_t)(int);
+typedef enum LoggerReturn { Error = -1, Exit , Continue, Success} logger_return_t;
 
 /******************************************************************************
 ***********************PRINT FUNCTION SECTION**********************************
@@ -25,7 +25,7 @@ typedef enum LoggerReturn { Error = -1, Exit , Continue, Success} logger_return;
 struct print_me
 {
 	int x;
-	print_func_type print_ptr;
+	print_func_t print_ptr;
 };
 
 /*Print function to be passed to the structure array*/
@@ -35,7 +35,7 @@ void Print(int x)
 }
 
 /*Creating a pointer to the Print() function*/
-print_func_type print_ptr = Print;
+print_func_t print_ptr = Print;
 
 /*Creating an Array of print_me structures*/
 struct print_me struct_prints[PRINT_ARR_SIZE];
@@ -73,8 +73,8 @@ void PrintInMain()
 struct special_inputs
 {
 	char *str;
-	compare_ptr comparison;
-	cmd_func operation;
+	compare_ptr_t comparison;
+	cmd_func_t operation;
 };
 
 /*=============================================
@@ -90,39 +90,34 @@ int GetFileLength(FILE *file)
 	return length;
 }
 
-/*Exits from the file*/
-logger_return OnExit(FILE *file, char* filename)
+/*Exits from the program*/
+logger_return_t OnExit(char* filename)
 {
 	(void)filename;
-	fclose(file);
 	return Exit;
 }
 
 /*Deletes the file*/
-logger_return OnRemove(FILE *file, char* filename)
+logger_return_t OnRemove(char* filename)
 {
-	fclose(file); 
 	remove(filename);
 	return Exit;
 }
 
-
-/*
-OnCount FUNCTION IS NOT CLOSING THE FILE FOR A PURPOSE.
-IT CONTINUES TO RUN NORMALLY AFTER WE PRINT TO THE USER THE NUMBER OF LINES !
-*/
-
 /*Returns the number of lines in the file, based on counting the newline character*/
-logger_return OnCount(FILE *file, char* filename)
+logger_return_t OnCount(char* filename)
 {
 	char c = 0;
 	int count = 0;
-	(void)filename;
+	
+	FILE *file = fopen(filename, "r");
+	if(NULL == file)
+	{
+		return Error;
+	}
 	
 	/*Moving the pointer to the beginning of the file*/
 	fseek(file, 0, SEEK_SET);
-	
-	/*Looping over the whole file, for every newline character, adding the counts*/
 	while (c != EOF )
 	{
 		c = fgetc(file);
@@ -131,6 +126,7 @@ logger_return OnCount(FILE *file, char* filename)
 			count++;
 		}
 	}
+	fclose(file);
 	
 	if( 0  <  count)
 	{
@@ -144,14 +140,17 @@ logger_return OnCount(FILE *file, char* filename)
 	return Continue;
 }
 
-/*
-	
-*/
-logger_return OnLeftArrow(FILE *file, char* filename)
+logger_return_t OnLeftArrow(char* filename)
 {
 	char *buffer = NULL;
 	int arrow_placement = 0;
 	int length = 0;
+	FILE *file = fopen(filename, "a+");
+	
+	if(NULL == file)
+	{
+		return Error;
+	}
 		
 	length = GetFileLength(file);
 	
@@ -172,59 +171,40 @@ logger_return OnLeftArrow(FILE *file, char* filename)
 	fread(buffer, sizeof(char), length, file);
 	
 	freopen(filename, "w+", file);
+	if(NULL == file)
+	{
+		return Error;
+	}
 	fwrite(buffer+arrow_placement, sizeof(char), length-arrow_placement ,file);
 	fclose(file);
 	
 	file = fopen(filename, "a");
+	if(NULL == file)
+	{
+		return Error;
+	}
 	fwrite(buffer, sizeof(char), arrow_placement-1 ,file);
 	fclose(file);
 	
 	free(buffer);
 	buffer = NULL;
 
-	return Exit;
-}
-
-/*
-	The functions checks which comnnad input the user sent,
-	by chain of responsibility type-of (loops over every untill one dealing 
-	with it)
-*/
-logger_return CheckCommand(char *command, struct special_inputs array[CMD_ARR_SIZE], FILE *file, char* filename)
-{
-	int result = 2;
-	size_t i = 0;
-
-	while(i < CMD_ARR_SIZE)
-	{
-		/*Checking if its the actual command (if it equals to the string completely)*/
-		result = array[i].comparison(command, array[i].str, strlen(array[i].str));
-		
-		/*in case it is the same - it triggers the operation in that struct*/
-		if(0 == result)
-		{
-			return array[i].operation(file, filename);
-		}
-		++i;
-	}
 	return Continue;
 }
-
 
 /*=============================================
          Initialization Block
 =============================================*/
 
-/*initialize the struct array*/
-struct special_inputs array_of_inputs[CMD_ARR_SIZE];
-
-/*initialize the function pointers*/
-compare_ptr compare = strncmp;
-cmd_func ext = OnExit;
-cmd_func rm = OnRemove;
-cmd_func count = OnCount;
-cmd_func write_on_top = OnLeftArrow;
-
+	/*initialize the function pointers*/
+	compare_ptr_t compare = strncmp;
+	cmd_func_t ext = OnExit;
+	cmd_func_t rm = OnRemove;
+	cmd_func_t count = OnCount;
+	cmd_func_t write_on_top = OnLeftArrow;
+	
+	struct special_inputs array_of_inputs[CMD_ARR_SIZE];
+	
 /*Filling the array previously created with its related functions & commands*/
 void FillSpecialInputs(struct special_inputs array[CMD_ARR_SIZE])
 {
@@ -244,40 +224,89 @@ void FillSpecialInputs(struct special_inputs array[CMD_ARR_SIZE])
 	array[3].comparison = compare;
 	array[3].operation = write_on_top;
 }
+/*
+	The functions checks which comnnad input the user sent,
+	by chain of responsibility type-of (loops over every untill one dealing 
+	with it)
+*/
+logger_return_t CheckCommand(char *input_string, struct special_inputs array[CMD_ARR_SIZE], char* filename)
+{
+	int result = Continue;
+	size_t i = 0;
+
+	while(i < CMD_ARR_SIZE)
+	{
+		/*Checking if its the actual command (if it equals to the string completely)*/
+		result = array[i].comparison(input_string, array[i].str, strlen(array[i].str));
+		
+		/*in case it is the same - it triggers the operation in that struct*/
+		if(Exit == result)
+		{
+			return array[i].operation(filename);
+		}
+		++i;
+	}
+	return Continue;
+}
+
+logger_return_t GetInput(char *filename, struct special_inputs array_of_inputs[CMD_ARR_SIZE])
+{	
+	char line_buffer[MAX_LINE_SIZE];
+	char *input_string = NULL;
+	int is_continue = Continue;
+	FILE *file;
+	
+	input_string = fgets(line_buffer, MAX_LINE_SIZE, stdin);
+		
+	if(0 == compare(input_string, "-", 1))
+	{
+		is_continue = CheckCommand(input_string, array_of_inputs, filename);
+		return is_continue;
+	}
+	else if(0 == compare(input_string, "<", 1))
+	{
+		file = fopen(filename, "a+");
+		
+		if(NULL == file)
+		{
+			return Error;
+		}
+		fwrite(line_buffer, sizeof(char), strlen(input_string), file);
+		fclose(file);
+		is_continue = CheckCommand(input_string, array_of_inputs, filename);
+
+		
+		return is_continue;
+	}
+	else
+	{
+		file = fopen(filename, "a+");
+		if(NULL == file)
+		{
+			return Error;
+		}
+		fwrite(line_buffer, sizeof(char), strlen(input_string), file);
+		fclose(file);
+		return Continue;
+	}
+	
+	return is_continue;
+}
 
 /*=============================================
          Logger Function Block
 =============================================*/
 int Logger(char *filename)
 {	
-	int cmd_flag = 1;
-	char line_buffer[MAX_LINE_SIZE];
-	char *input_string = NULL;
-	FILE *fp = fopen(filename, "a+");
+	int is_running = Continue;
 	
 	FillSpecialInputs(array_of_inputs);
-	
-	if(NULL == fp)
-		return Error;
 		
 	printf("Let's write a book!\n");
 	
-	while(1 == cmd_flag)
+	while(Continue == is_running)
 	{
-		input_string = fgets(line_buffer, MAX_LINE_SIZE, stdin);
-		if(0 == compare(input_string, "-", 1))
-		{
-			cmd_flag = CheckCommand(input_string, array_of_inputs, fp, filename);
-		}
-		else if(0 == compare(input_string, "<", 1))
-		{
-			fwrite(line_buffer, sizeof(char), strlen(input_string), fp);
-			cmd_flag = CheckCommand(input_string, array_of_inputs, fp, filename);
-		}
-		else
-		{
-			fwrite(line_buffer, sizeof(char), strlen(input_string), fp);
-		}
+		is_running = GetInput(filename, array_of_inputs);
 	}	
 	return Success;
 }
--- c/tst.c
+++ c/tst.c
@@ -1,196 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#define CMD_ARR_SIZE (4)
-#define MAX_LINE_SIZE (200)
-
-
-
-
-typedef int (*cmd_func)(FILE *file, char* filename);
-typedef int (*compare_ptr)(const char*, const char*, size_t);
-typedef void (*func_ptr)(int);
-typedef int (*fun)(char *);
-
-int OnExit(FILE *file, char* filename)
-{
-	(void)filename;
-	return fclose(file);
-}
-
-int OnRemove(FILE *file, char* filename)
-{
-	fclose(file); 
-	remove(filename);
-	return 0;
-}
-
-int OnCount(FILE *file, char* filename)
-{
-	char c = 0;
-	int count = 0;
-	(void)filename;
-
-	fseek(file, 0, SEEK_SET);
-	
-	while (c != EOF )
-	{
-		c = fgetc(file);
-		if(c == '\n')
-		{
-			count++;
-		}
-	}
-	
-	if( 0 < count)
-	{
-		printf("Number of lines in the file is %d\n", count);
-	}
-	else
-	{
-		printf("The file is empty !\n");
-
-	}
-	return 1;
-}
-
-int OnLeftArrow(FILE *file, char* filename)
-{
-	char *buffer = NULL;
-	int arrow_placement = 0;
-	int length = 0;
-		
-	fseek(file, 0, SEEK_END);
-	length = ftell(file);
-	fseek(file, 0, SEEK_SET);
-	
-	buffer = (char *)malloc(length);
-	if(NULL == buffer)
-	{
-		return 0;
-	}
-	
-	
-	while(arrow_placement != '<')
-	{
-		arrow_placement = fgetc(file);
-	}
-	
-	arrow_placement = ftell(file);
-	fseek(file, 0, SEEK_SET);
-	
-	fread(buffer, 1, length, file);
-	
-	freopen(filename, "w+", file);
-	
-	printf("%s\n", buffer+arrow_placement);
-	
-	fwrite(buffer+arrow_placement, 1, length-arrow_placement ,file);
-	
-	fclose(file);
-	file = fopen(filename, "a");
-	
-	fwrite(buffer, 1, arrow_placement-1 ,file);
-	
-	fclose(file);
-	
-	free(buffer);
-	buffer = NULL;
-
-	return 1;
-}
-
-	compare_ptr compare = strncmp;
-	cmd_func ext = OnExit;
-	cmd_func rm = OnRemove;
-	cmd_func count = OnCount;
-	cmd_func write_on_top = OnLeftArrow;
-	
-struct special_inputs
-{
-	char *str;
-	compare_ptr comparison;
-	fun operation;
-};
-
-struct special_inputs array_of_inputs[CMD_ARR_SIZE];
-
-void FillArr(struct special_inputs array[CMD_ARR_SIZE])
-{
-	array[0].str = "-count\n";
-	array[0].comparison = compare;
-	array[0].operation = count;
-	
-	array[1].str = "-remove\n";
-	array[1].comparison = compare;
-	array[1].operation = rm;
-	
-	array[2].str = "-exit\n";
-	array[2].comparison = compare;
-	array[2].operation = ext;
-	
-	array[3].str = "<";
-	array[3].comparison = compare;
-	array[3].operation = write_on_top;
-}
-
-int CheckCommand(char *command, struct special_inputs array[CMD_ARR_SIZE], FILE *file, char* filename)
-{
-	int result = 2;
-	size_t i = 0;
-
-	while(i < CMD_ARR_SIZE)
-	{
-		result = array[i].comparison(command, array[i].str, strlen(array[i].str));
-		if(0 == result)
-		{
-			return array[i].operation(file, filename);
-		}
-		++i;
-	}
-	return 1;
-}
-
-
-int Logger(char *filename)
+/*When y is a Negative value - the behavior will be undefined*/
+long Pow2(unsigned int x, unsigned int y)
 {	
-
-	int cmd_flag = 1;
-	char line_buffer[MAX_LINE_SIZE];
-	char *input_string = NULL;
-	FILE *fp = fopen(filename, "a+");
-	
-	FillArr(array_of_inputs);
-	
-	if(NULL == fp)
-		return -1;
-		
-	printf("Let's write a book!\n");
-	
-	while(1 == cmd_flag)
-	{
-		input_string = fgets(line_buffer, MAX_LINE_SIZE, stdin);
-		if(0 == compare(input_string, "-", 1))
-		{
-			cmd_flag = CheckCommand(input_string, array_of_inputs, fp, filename);
-		}
-		else if(0 == compare(input_string, "<", 1))
-		{
-			fwrite(line_buffer, sizeof(char), strlen(input_string), fp);
-			cmd_flag = CheckCommand(input_string, array_of_inputs, fp, filename);
-		}
-		else
-		{
-			fwrite(line_buffer, sizeof(char), strlen(input_string), fp);
-		}
-	}	
-	return 1;
+	return 0 == y ? x : x*(2<<(y-1));
 }
+
+long IsPow2Loop(unsigned int n)
+{
+	while(n > 3 && n%2 == 0){
+		n>>=1;
+	}
+	return 2 == n ? 1 : 0;
+}
+
+long IsPow2(unsigned int n)
+{
+	/*If bits are different, then the result is 1, else 0*/
+	return (n+2 == (n^2)) && (n % 2 == 0) ? 1 : 0;
+}
+
+long AddOne(int n )
+{
+	if(n%2==0){
+		return n = n | 1;}
+		
+		 n = n ^(~n &~1);
+	return n;
+	
+}
+
 
 
 int main()
-{	
-	Logger("tst.txt");
+{
+	printf("Answer %ld\n", AddOne(3));
+	printf("Answer %ld\n", AddOne(4));
+	printf("Answer %ld\n", AddOne(64));
+	printf("Answer %ld\n", AddOne(33));
+	printf("Answer %ld\n", AddOne(512));
+	printf("Answer %ld\n", AddOne(514));
 	return 0;
 }
