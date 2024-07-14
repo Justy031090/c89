7a113111b88dad7975732773e92013e8b2dfefd9
Michael.Bar <justy031090@gmail.com>
2024-07-14 14:57:22 +0300

    append implementation & test

--- ds/src/queue.c
+++ ds/src/queue.c
@@ -4,7 +4,9 @@
 (")-("))	..
 */
 
-#include <sdtdef.h> /*size_t*/
+#include <assert.h> /*assert*/
+#include <stdlib.h> /*malloc*/
+#include <stddef.h> /*size_t*/
 #include "queue.h"
 
 
@@ -24,41 +26,44 @@ struct sll
 };
 struct queue
 {
-    sll_t *list;
+    sll_t *sll;
 };
 
 
 queue_t *QueueCreate(void)
 {
-	sll_t *new_queue = malloc(sizeof(queue_t));
+	queue_t *new_queue = malloc(sizeof(queue_t));
 	if(NULL == new_queue)
 	{
 		return NULL;
 	}
-	new_queue->list = malloc(sizeof(sll_t));
-	if(NULL == new_queue->list)
+	new_queue->sll = malloc(sizeof(sll_t));
+	if(NULL == new_queue->sll)
 	{
 		free(new_queue);
 		new_queue = NULL;
 		return NULL;
 	}
+	new_queue->sll->head = NULL;
+	new_queue->sll->tail = NULL;
+	
 	return new_queue;
 }
 
 void *QueuePeek(const queue_t *queue)
 {
-	return queue->list->tail;
+	return queue->sll->tail->data;
 }
 
 int QueueIsEmpty(const queue_t *queue)
 {
-	return queue->list->head == queue->list->tail ? 1 : 0;
+	return queue->sll->head == queue->sll->tail ? 1 : 0;
 }
 
 size_t QueueSize(const queue_t *queue)
 {
 	size_t counter = 0;
-	node_t current = queue->sll->head;
+	node_t *current = queue->sll->head;
 	
 	while(NULL != current)
 	{
@@ -73,28 +78,33 @@ int Enqueue(queue_t *queue, const void *val)
 	node_t *new_node = malloc(sizeof(node_t));
 	if(NULL == new_node)
 	{
-		return NULL;
+		return 0;
 	}
-	
-	queue->tail->next = new_node;
-	queue->tail = new_node;
-	queue->tail->data = (void *val);
+	if(queue->sll->head == NULL)
+	{
+		queue->sll->head = new_node;
+		queue->sll->tail = new_node;
+	}
+	queue->sll->tail->next = new_node;
+	queue->sll->tail = new_node;
+	new_node->data = (void *)val;
+	new_node->next = NULL;
 	
 	return 1;
 }
 
 void Dequeue(queue_t *queue)
 {
-	node_t tmp = queue->list->head;
-	queue->list->head = tmp->next;
+	node_t *tmp = queue->sll->head;
+	queue->sll->head = tmp->next;
 	free(tmp);
-	queue->list->head = NULL;
+	tmp = NULL;
 }
 
 void QueueDestroy(queue_t *queue)
 {
-	node_t current = queue->list->head;
-	node_t next = queue->list->head->next;
+	node_t *current = queue->sll->head;
+	node_t *next = queue->sll->head->next;
 	while(NULL != next)
 	{
 		free(current);
@@ -109,11 +119,11 @@ void QueueDestroy(queue_t *queue)
 
 queue_t *QueueApend(queue_t *queue_src, queue_t *queue_dest)
 {
-	queue_dest->list->tail->next = queue_src->list->head;
-	queue_dest->list->tail = queue_src->list->tail;
-	queue_src->list->head = NULL;
-	queue_src->list->tail = NULL;
-	
+	queue_dest->sll->tail->next = queue_src->sll->head;
+	queue_dest->sll->tail = queue_src->sll->tail;
+	queue_src->sll->head = NULL;
+	queue_src->sll->tail = NULL;
+
 	return queue_dest;
 }
 
--- ds/test/queue_test.c
+++ ds/test/queue_test.c
@@ -1,8 +1,39 @@
 
-
-
+#include <stdio.h>
+#include "queue.h"
 
 int main()
 {
+queue_t *queue1 = QueueCreate();
+queue_t *queue2 = QueueCreate();
+
+int x = 8, y = 9, z = 10;
+
+Enqueue(queue1, &x);
+printf("Size 1 %lu\n", QueueSize(queue1));
+Enqueue(queue1, &y);
+Enqueue(queue1, &z);
+
+
+Dequeue(queue1);
+
+printf("%d\n", *(int *)QueuePeek(queue1));
+
+printf("Empty %d\n", QueueIsEmpty(queue2));
+printf("Not Empty %d\n", QueueIsEmpty(queue1));
+
+printf("Size %lu\n", QueueSize(queue1));
+Enqueue(queue2, &y);
+Enqueue(queue2, &z);
+
+printf("Size q1 %lu\n", QueueSize(queue1));
+printf("Size q2 %lu\n", QueueSize(queue2));
+queue1 = QueueApend(queue2,queue1);
+
+printf("Size q1 %lu\n", QueueSize(queue1));
+printf("Size q2 %lu\n", QueueSize(queue2));
+
+
+/*void QueueDestroy(queue_t *queue);*/
 	return 0;
 }
