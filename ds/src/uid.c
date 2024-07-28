f492380996545e9d0f1d23b161181bb4e7c215c6
Michael.Bar <justy031090@gmail.com>
2024-07-28 15:36:02 +0300

    added CR lines

--- ds/src/uid.c
+++ ds/src/uid.c
@@ -1,16 +1,23 @@
-#include "uid.h"
-#include <unistd.h>
+/*			.. UID Generator Implementation ..
+(\.../)		.. Authored by Michael Bar 28/07/2024 .. 
+(=';'=) 	.. code reviewd by ..
+(")-("))	.. The only hard day was yesterday ! ..
+*/
 
+#include "uid.h"
 
 const my_uid_t bad_uid = {0,0,0,0};
 
 my_uid_t UIDGenerate()
 {
-	static size_t counter = 1;
-	time_t sec = 0;
 	my_uid_t new_uid;
+	static size_t counter = 1;
+	time_t sec = time(&sec);
+	if(0 > sec)
+		return bad_uid;
+	
 	new_uid.counter = counter;
-	new_uid.timestamp = time(&sec);
+	new_uid.timestamp = sec;
 	new_uid.pid = getpid();
 	new_uid.ip = gethostid();
 	
