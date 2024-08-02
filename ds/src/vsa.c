21da0ed37057e3f52bddf48caa563c0e215683f3
Michael.Bar <justy031090@gmail.com>
2024-08-02 18:35:56 +0300

    vsa implemented

--- ds/src/vsa.c
+++ ds/src/vsa.c
@@ -1,10 +1,11 @@
 /*			.. Variable Memory Allocator Implementation ..
-(\.../)		.. Authored by Michael Bar 28/07/2024 .. 
+(\.../)		.. Authored by Michael Bar 02/08/2024 .. 
 (=';'=) 	.. code reviewd by ..
 (")-("))	.. The only hard day was yesterday ! ..
 */
 
 #include <stddef.h>/*size_t*/
+#include <math.h>
 
 #include "vsa.h"
 
@@ -13,31 +14,95 @@ struct vsa
 	long mem_count;
 };
 
+static long _Abs (long x);
+
+static vsa_t *_JumpNext(vsa_t *index);
+
+static void _FragmentationFix(vsa_t *runner, vsa_t *next_index);
+
+
+
 vsa_t *VSAInit(void *memory_pool, size_t mem_size)
 {
+	vsa_t *first_index = (vsa_t *)memory_pool;
 	vsa_t *last_index = NULL;
-	((vsa_t *)memory_pool)->mem_count = mem_size - (2 * (sizeof(vsa_t)));
-	last_index = ((vsa_t *)(char *)memory_pool + mem_size - sizeof(vsa_t));
+	first_index->mem_count = mem_size - (2 * (sizeof(vsa_t)));
+	last_index = ((vsa_t *)(char *)memory_pool + (mem_size-1) - sizeof(vsa_t));
 	last_index->mem_count = 0;  
 		
 	return (vsa_t *)memory_pool;
 }
 
-void *VSAAllocate(vsa_t *vsa, size_t size_of_block)
-{
-	while(vsa->mem_count < size_of_block || vsa->mem_count == 0)
-	{
-		vsa = vsa + mem_count +1;
-				
-	}
-}
-
 void VSAFreeBlock(void *to_free)
 {
-	
-}
-size_t VSALargestFreeBlock(const vsa_t *vsa)
-{
-	return 0;
+	((vsa_t *)((char *)to_free - sizeof(vsa_t)))->mem_count *= -1;
 }
 
+void *VSAAllocate(vsa_t *vsa, size_t size_of_block)
+{
+	vsa_t *runner = vsa;
+	vsa_t *next_index = _JumpNext(runner);
+	
+	while(next_index->mem_count != 0)
+	{	
+		if(runner->mem_count > 0 && next_index->mem_count > 0)
+		{
+				_FragmentationFix(runner, next_index);
+				continue;		
+		}
+		
+		if(runner->mem_count < 0 || runner->mem_count < (long)size_of_block)
+		{
+			runner = _JumpNext(runner);
+			next_index = _JumpNext(runner);
+		}
+		
+		else
+		{
+			runner->mem_count = size_of_block + sizeof(vsa_t);
+		}
+	}
+	
+	if(next_index->mem_count == 0 && (runner->mem_count < 0 || runner->mem_count < (long)size_of_block))
+		return NULL;
+	
+	return (void *)(runner + sizeof(vsa_t));		
+}		
+
+
+size_t VSALargestFreeBlock(const vsa_t *vsa)
+{
+	vsa_t *runner = (vsa_t *)vsa;
+	vsa_t *next_index = _JumpNext(runner);
+	size_t largest_block = 0;
+	
+	while(next_index->mem_count != 0)
+	{
+		if(runner->mem_count > (long)largest_block)
+		{
+			largest_block = runner->mem_count;
+		}
+		if(runner->mem_count > 0 && next_index->mem_count > 0)
+		{
+			_FragmentationFix(runner, next_index);
+			continue;		
+		}
+	}
+	return largest_block;
+}
+
+static long _Abs (long x)
+{
+	return x >= 0 ? x : (-x);
+}
+
+static vsa_t *_JumpNext(vsa_t *index)
+{
+	return index + _Abs(index->mem_count);
+}
+
+static void _FragmentationFix(vsa_t *runner, vsa_t *next_index)
+{
+	runner->mem_count = runner->mem_count + next_index->mem_count;
+	next_index = _JumpNext(runner);		
+}
--- ds/test/vsa_test.c
+++ ds/test/vsa_test.c
@@ -1,5 +1,28 @@
+#include <printf.h>
+#include <stdlib.h>
+
+
+#include "vsa.h"
+
 int main ()
 {
+	vsa_t *vsa = NULL;
+	size_t *pool = malloc(200);
+	if(!pool)
+		return 1;
+		
+	vsa = VSAInit(pool, 200);
+
 	
+	
+	
+	free(pool);
+
 	return 0;
 }
+
+
+
+void *VSAAllocate(vsa_t *vsa, size_t size_of_block);
+void VSAFreeBlock(void *to_free);
+size_t VSALargestFreeBlock(const vsa_t *vsa);
