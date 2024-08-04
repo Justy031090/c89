9b2fed831f3428b2c0a7f207d0ed3cd8daed2d85
Michael.Bar <justy031090@gmail.com>
2024-08-04 16:05:35 +0300

    refactoring

--- ds/src/fsa.c
+++ ds/src/fsa.c
@@ -14,11 +14,12 @@ struct fsa
     size_t next_free; 
 };
 
-static size_t CheckBlockSize(size_t block_size);
+static size_t _AlignBlock(size_t size_of_block);
 
 size_t FSAMinPoolSize(size_t size_of_block, size_t num_of_blocks)
 {
-		size_t block = CheckBlockSize(size_of_block);
+		size_t block = _AlignBlock(size_of_block);
+		assert(0 != num_of_blocks);
 		return (block*num_of_blocks) + sizeof(fsa_t);
 }
 
@@ -36,7 +37,9 @@ void *FSAAllocateBlock(fsa_t *fsa)
 size_t FSACountNumOfFreeBlocks(const fsa_t *fsa)
 {
 	size_t count = 0;
-	size_t runner = fsa->next_free;
+	size_t runner = 0;
+	assert(NULL != fsa);
+	runner = fsa->next_free;
 	
 	while(0 != runner)
 	{
@@ -48,7 +51,10 @@ size_t FSACountNumOfFreeBlocks(const fsa_t *fsa)
 
 void FSAFreeBlock(fsa_t *fsa, void *to_free)
 {
-	size_t *free_block = to_free;
+	size_t *free_block = NULL;
+	assert(NULL != fsa);
+	assert(NULL != to_free);
+	free_block = (size_t *)to_free;
 	*free_block = fsa->next_free;
 	fsa->next_free = ((size_t)fsa ^ (size_t)to_free);
 }
@@ -57,7 +63,8 @@ fsa_t *FSAInit(void *memory_pool, size_t size_of_block, size_t num_of_blocks)
 {
 	size_t i = 0;
 	size_t *insert =(size_t *)memory_pool + 1;
-	size_t block = CheckBlockSize(size_of_block);
+	size_t block = _AlignBlock(size_of_block);
+	assert(0 != num_of_blocks);
 	
 	((fsa_t *)memory_pool)->next_free = BLOCK_SIZE;
 	
@@ -71,10 +78,10 @@ fsa_t *FSAInit(void *memory_pool, size_t size_of_block, size_t num_of_blocks)
 	return (fsa_t *)memory_pool;
 }
 
-static size_t CheckBlockSize(size_t block_size)
+static size_t _AlignBlock(size_t size_of_block)
 {
-	return block_size < BLOCK_SIZE ? BLOCK_SIZE : block_size;
+	size_of_block = (size_of_block + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1 );
+	return size_of_block;
 }
-
 
 
