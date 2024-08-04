be8c9ec3b990a248a47c2227a669c9d544010f81
Michael.Bar <justy031090@gmail.com>
2024-08-04 12:42:28 +0300

    vsa tests

--- ds/src/vsa.c
+++ ds/src/vsa.c
@@ -14,27 +14,31 @@
 struct vsa
 {
 	long mem_count;
+	#ifndef NDBUG
+	size_t magic_num;
+	#endif
 };
 
 static long _Abs (long x);
 static vsa_t *_JumpNext(vsa_t *index);
-static void _FragmentationFix(vsa_t *runner, vsa_t *next_index);
+static long _FragmentationFix(vsa_t *runner, vsa_t *next_index);
 static size_t _AlignBlock(size_t size_of_block);
 
 vsa_t *VSAInit(void *memory_pool, size_t mem_size)
 {
 	vsa_t *first_index = NULL;
 	vsa_t *last_index = NULL;
+	size_t two_headers = sizeof(vsa_t) * 2;
 	
-	if(NULL == memory_pool || mem_size < sizeof(vsa_t) * 2)
+	if(NULL == memory_pool || mem_size < two_headers+ALIGNMENT)
 	{
 		return NULL;
 	}
 	
 	first_index = (vsa_t *)memory_pool;
-	last_index = (vsa_t *)((char *)memory_pool + mem_size - sizeof(vsa_t));
+	last_index = (vsa_t *)((char *)memory_pool + mem_size - ALIGNMENT);
 	
-	first_index->mem_count = mem_size - sizeof(vsa_t);
+	first_index->mem_count = mem_size - two_headers;
 	last_index->mem_count = 0;  
 		
 	return first_index;
@@ -45,7 +49,7 @@ void VSAFreeBlock(void *to_free)
 	vsa_t *free_index = NULL;
 	assert(NULL != to_free);
 	
-	free_index = (vsa_t *)((char *)to_free - sizeof(vsa_t));
+	free_index = (vsa_t *)((char *)to_free - ALIGNMENT);
 	free_index->mem_count = _Abs(free_index->mem_count);
 }
 
@@ -53,35 +57,46 @@ void *VSAAllocate(vsa_t *vsa, size_t size_of_block)
 {
 	vsa_t *runner = vsa;
 	vsa_t *next_index = _JumpNext(runner);
+	long temp;
+	
 	size_of_block = _AlignBlock(size_of_block);
 	
-	while(0 != next_index->mem_count)
-	{	
-		if(runner->mem_count > 0 && next_index->mem_count > 0)
+	while (0 != (next_index)->mem_count && (runner->mem_count < (long)size_of_block) && runner->mem_count != 0)
+	{
+		if(runner->mem_count < 0)
 		{
-				_FragmentationFix(runner, next_index);
-				next_index = _JumpNext(runner);
-				continue;		
+			runner = (vsa_t *)(((char *)runner) + _Abs(runner->mem_count) + ALIGNMENT);
 		}
-		
-		if(runner->mem_count < 0 || runner->mem_count < (long)size_of_block)
-		{
-			runner = _JumpNext(runner);
-			next_index = _JumpNext(runner);
-		}
-		
 		else
 		{
-			runner->mem_count = size_of_block + sizeof(vsa_t);
-			return (void *)((char *)runner + sizeof(vsa_t));
+			if (((vsa_t *)(((char *)runner) +runner->mem_count + ALIGNMENT))->mem_count > 0)
+			{
+				runner->mem_count += ((vsa_t *)(((char *)runner) +runner->mem_count + ALIGNMENT))->mem_count +ALIGNMENT;
+			}
+			else
+			{
+				runner = (vsa_t *)(((char *)runner) + _Abs(runner->mem_count) + ALIGNMENT);
+			}
 		}
 	}
 	
-	if(next_index->mem_count == 0 && (runner->mem_count < 0 || runner->mem_count < (long)size_of_block))
+	if(runner->mem_count >= ((long)size_of_block))
+	{	
+		if (runner->mem_count != (long)size_of_block)
+		{
+			temp = runner->mem_count - ALIGNMENT - size_of_block;
+			((vsa_t *)(((char *)runner) + size_of_block + ALIGNMENT))->mem_count = temp;
+		}
+		runner->mem_count = -size_of_block;
+		return (void *)(((char *)runner)+ ALIGNMENT);
+	}
+	else
+	{
 		return NULL;
+	}
+
+}
 	
-	return NULL;		
-}		
 
 
 size_t VSALargestFreeBlock(const vsa_t *vsa)
@@ -90,21 +105,31 @@ size_t VSALargestFreeBlock(const vsa_t *vsa)
 	vsa_t *next_index = _JumpNext(runner);
 	long largest_block = 0;
 	
-	while(next_index->mem_count != 0)
+	while( 0 != (next_index)->mem_count && 0 != runner->mem_count)
 	{
-		if(runner->mem_count > largest_block)
+		if(runner->mem_count < 0)
 		{
-			largest_block = runner->mem_count;
-		}
-		if(runner->mem_count > 0 && next_index->mem_count > 0)
-		{
-			_FragmentationFix(runner, next_index);
+			runner = _JumpNext(runner);
 			next_index = _JumpNext(runner);
-			continue;		
 		}
 		
-		runner = _JumpNext(runner);
-		next_index = _JumpNext(runner);
+		
+		else
+		{
+			if((next_index+ALIGNMENT)->mem_count >0)
+			{
+				runner->mem_count = _FragmentationFix(runner, (next_index));
+			}
+			else
+			{
+				if(largest_block < runner->mem_count)
+					largest_block = runner->mem_count;
+					
+				runner = _JumpNext(runner);
+				next_index = _JumpNext(runner);
+			}
+		}
+			
 	}
 	return largest_block;
 }
@@ -116,16 +141,16 @@ static long _Abs (long x)
 
 static vsa_t *_JumpNext(vsa_t *index)
 {
-	return (vsa_t *)((char *)index + _Abs(index->mem_count));
+	return (vsa_t *)((char *)index + _Abs(index->mem_count) + ALIGNMENT);
 }
 
-static void _FragmentationFix(vsa_t *runner, vsa_t *next_index)
+static long _FragmentationFix(vsa_t *runner, vsa_t *next_index)
 {
-	runner->mem_count = runner->mem_count + next_index->mem_count;	
+	return runner->mem_count += next_index->mem_count + ALIGNMENT;	
 }
 
 static size_t _AlignBlock(size_t size_of_block)
 {
 	size_of_block = (size_of_block + ALIGNMENT - 1) & ~(ALIGNMENT - 1 );
-	return size_of_block >= MIN_BLOCK_SIZE ? size_of_block : MIN_BLOCK_SIZE;
+	return size_of_block >= ALIGNMENT ? size_of_block : ALIGNMENT;
 }
--- ds/test/vsa_test.c
+++ ds/test/vsa_test.c
@@ -1,10 +1,69 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> /*printf*/
 #include "vsa.h"
+int main()
+{
+	char mem_block[128];
+	vsa_t *new_vsa = VSAInit((void *) mem_block, 128);
+	void *a = NULL;
+	void *b = NULL;
+	void *c = NULL;
+	void *d = NULL;
+	void *e = NULL;
+	if ((*((size_t *)new_vsa)) = 48)
+	{
+		printf("Init success\n");
+	}
+	else
+	{
+		printf("Init FAIL!!");
+	}
+	
+	a = VSAAllocate(new_vsa, 7);
+	b = VSAAllocate(new_vsa, 15);
+	c = VSAAllocate(new_vsa, 7);
+	d = VSAAllocate(new_vsa, 17);/*NULL*/
 
-
-int main() {
-
-    return 0;
+	if (NULL != c && NULL != a && NULL != b)
+	{
+		printf("Allocate success\n");
+	}
+	else
+	{
+		printf("Allocate FAIL!!\n");
+	}
+	
+	if (0 == VSALargestFreeBlock(new_vsa))
+	{
+		printf("LargestFreeBlock full success\n");
+	}
+	else
+	{
+		printf("LargestFreeBlock full FAIL!!\n");
+	}
+	
+	VSAFreeBlock(a);
+	
+	if (8 == VSALargestFreeBlock(new_vsa))
+	{
+		printf("Free success\n");
+		printf("LargestFreeBlock notfull success\n");
+	}
+	d = VSAAllocate(new_vsa, 7);
+	if (0 == VSALargestFreeBlock(new_vsa))
+	{
+		printf("ReAllocate success\n");
+	}
+	else
+	{
+		printf("ReAllocate FAIL!!\n");
+	}
+	VSAFreeBlock(a);
+	VSAFreeBlock(a);
+	e = VSAAllocate(new_vsa, 17);
+	if (e != NULL && 0 == VSALargestFreeBlock(new_vsa))
+	{
+		printf("ReAllocate full  success\n");
+	}
+	return 0;
 }
 
