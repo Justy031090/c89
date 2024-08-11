14a255203938c0d6e4674496533026ba9266e8d3
Michael.Bar <justy031090@gmail.com>
2024-08-11 15:47:10 +0300

    further imp

--- ds/src/bst.c
+++ ds/src/bst.c
@@ -1,6 +1,10 @@
 #include <stddef.h>
 #include "bst.h"
 
+#define EQUAL (3)
+#define SMALLER (2)
+#define BIGGER (1)
+
 struct bst
 {
     node_t root_dummy;
@@ -20,7 +24,7 @@ enum children {
     NUM_OF_CHILD = 2
 };
 
-
+static void AddChildrenCount(bst_iter_t iter);
 bst_t *BSTCreate(compare_func_t cmp_func)
 {
     bst_t *new_bst = malloc(sizeof(bst_t));
@@ -41,23 +45,67 @@ bst_t *BSTCreate(compare_func_t cmp_func)
 
 void BSTDestroy(bst_t *bst)
 {
+    bst_iter_t first_node = NULL;
+    size_t children = bst->root_dummy.child_node[NUM_OF_CHILD];
+    while(children)
+    {
+        first_node = bst->root_dummy.child_node[LEFT];
+        BSTRemove(first_node);
+        --children;
+    }
     free(bst);
 }
 
 bst_iter_t Insert(bst_t *bst, const void *data)
 {
-    bst_iter_t check_if_exist = BSTFind(bst, data);
-    if (check_if_exist != NULL)
-    {
+    bst_iter_t to_insert = malloc(sizeof(node_t));
+    if(NULL == to_insert)
         return NULL;
-    }
-    /*check where to insert it*/
+    bst_iter_t first_node = bst->root_dummy.child_node[LEFT];
+    int is_position = 0;
+    int temp = 0;
+    to_insert->data = data;
 
+    while(!is_position)
+    {
+        temp = bst->cmp_func(first_node->data, data);
+        if(temp == EQUAL)
+            return NULL;
+
+        else if(temp == SMALLER)
+        {
+            first_node = first_node->child_node[LEFT];
+            continue;
+        }
+        else if(temp == BIGGER)
+        {
+            first_node->parent->child_node[LEFT] = to_insert;
+            to_insert->child_node[LEFT] = first_node;
+            AddChildrenCount(to_insert);
+        }
+    }
+
+return to_insert;
 }
 
 bst_iter_t BSTRemove(bst_iter_t iter)
 {
-
+    bst_iter_t childR = iter->child_node[RIGHT];
+    bst_iter_t childL = iter->child_node[LEFT];
+    if(iter->parent->data > iter->data)
+    {
+        iter->child_node[LEFT] = childR;
+        childR->child_node[LEFT] = childL;
+        iter->parent->child_node[LEFT] = childR;
+    }
+    else
+    {
+        iter->parent->child_node[RIGHT] = childL;
+        childL->child_node[RIGHT] = childR;
+    }
+    RemoveChildrenCount(iter);
+    free(iter);
+    return BSTNext(iter);
 }
 
 bst_iter_t BSTFind(const bst_t *bst, const void *data)
@@ -119,4 +167,21 @@ bst_iter_t BSTEnd(const bst_t *bst)
 int BSTIsEqual(bst_iter_t iter1, bst_iter_t iter2)
 {
     return iter1 == iter2;
+}
+
+static void AddChildrenCount(bst_iter_t iter)
+{
+    while(iter->parent != NULL)
+    {
+        iter->child_node[NUM_OF_CHILD]++;
+        iter = iter->parent;
+    }
+}
+static void RemoveChildrenCount(bst_iter_t iter)
+{
+    while(iter->parent != NULL)
+    {
+        iter->child_node[NUM_OF_CHILD]--;
+        iter = iter->parent;
+    }
 }
