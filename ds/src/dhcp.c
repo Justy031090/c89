5e6ff33cd27306035eac1a8b0804b1a9f35294b8
Michael.Bar <justy031090@gmail.com>
2024-09-02 09:45:05 +0300

    allocate fixes

--- ds/src/dhcp.c
+++ ds/src/dhcp.c
@@ -1,14 +1,6 @@
-/**************************************************************|	
-|		    .. DHCP via trie Implementation ..         ********|
-|  (\.../)	.. Authored by Michael Bar 31//08/2024 ..  ********|
-|  (=';'=) 	.. code reviewd by TBD 30.08.2024..        ********|
-|  (")-("))	.. The only hard day was yesterday ! ..    ********|
-***************************************************************/
-
-
-#include <stdlib.h> /*malloc*/
-#include <stddef.h> /*size_t*/
-#include <string.h> /*memcpy, memcmp*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 
 #include "dhcp.h"
@@ -16,7 +8,7 @@
 #define BITS_IN_BYTE 8
 #define TOTAL_BITS 32
 #define OCTET_MAX 255
-#define PRE_ALLOCATED 2
+#define PRE_ALLOCATED 2 
 
 enum children {
     LEFT = 0,
@@ -24,12 +16,6 @@ enum children {
     NUMBER_OF_CHILD = 2
 };
 
-struct dhcp {
-    node_t *root;
-    size_t mask;
-    unsigned char subnet[SUBNET_BYTES];
-    
-};
 
 struct node {
     int is_full;
@@ -37,22 +23,29 @@ struct node {
     node_t *children[NUMBER_OF_CHILD];
 };
 
+struct dhcp {
+    node_t *root;
+    size_t mask;
+    unsigned char subnet[SUBNET_BYTES];
+};
 
 static node_t *CreateNode(node_t *parent);
 static void FreeNode(node_t *node);
 static size_t CountFreeNodes(node_t *node);
-static node_t *FindSmallest(node_t *node);
 static void GetIpFromNode(node_t *node, unsigned char *ip, size_t mask);
+static int IsReservedIp(const unsigned char *ip, const unsigned char *subnet, size_t mask);
+static int FindSmallestAvailableIp(node_t *node, unsigned char *smallest_ip, size_t mask);
+static int FindNextClosestAvailableIp(node_t *node, const unsigned char *ip, unsigned char *next_ip, size_t mask);
+static node_t *FindSmallest2(node_t *parent);
 
+dhcp_t *DHCPCreate(const unsigned char subnet[SUBNET_BYTES], size_t mask) {
+    dhcp_t *dhcp;
 
-dhcp_t *DHCPCreate(const unsigned char subnet[SUBNET_BYTES],  size_t mask)
-{
-    dhcp_t *dhcp = malloc(sizeof(dhcp_t));
-    if(NULL == dhcp) return NULL;
+    dhcp = (dhcp_t *)malloc(sizeof(dhcp_t));
+    if (NULL == dhcp) return NULL;
 
     dhcp->root = CreateNode(NULL);
-    if(NULL == dhcp->root)
-    {
+    if (NULL == dhcp->root) {
         free(dhcp);
         return NULL;
     }
@@ -63,110 +56,95 @@ dhcp_t *DHCPCreate(const unsigned char subnet[SUBNET_BYTES],  size_t mask)
     return dhcp;
 }
 
-void DHCPDestroy(dhcp_t *dhcp)
-{
+void DHCPDestroy(dhcp_t *dhcp) {
     assert(NULL != dhcp);
     FreeNode(dhcp->root);
     free(dhcp);
 }
 
-size_t CountFreeIps(const dhcp_t *dhcp)
-{   
-    size_t total_ips = 0;
-    size_t used_ips = 0;
-    size_t sum = 0;
+size_t CountFreeIps(const dhcp_t *dhcp) {
+    size_t total_ips;
+    size_t used_ips;
+
     assert(NULL != dhcp);
 
     total_ips = (1 << (TOTAL_BITS - dhcp->mask));
     used_ips = CountFreeNodes(dhcp->root);
-    if(used_ips >1) used_ips -= 30;
-    sum = (total_ips - PRE_ALLOCATED - (used_ips));
-    return sum;
+
+    return total_ips - PRE_ALLOCATED - used_ips;
 }
 
 int AllocateIp(dhcp_t *dhcp, const unsigned char ip[SUBNET_BYTES], unsigned char dest_ip[SUBNET_BYTES]) {
     node_t *current = dhcp->root;
-    node_t *smallest = NULL;
-    unsigned char current_ip[SUBNET_BYTES] = {0};
     unsigned char cpy_ip[SUBNET_BYTES];
     size_t i, bit;
     int bit_val;
-    
-    memcpy(cpy_ip, ip, SUBNET_BYTES);
-    if(memcmp(cpy_ip, dhcp->subnet, SUBNET_BYTES) == 0)
-    {
-        cpy_ip[SUBNET_BYTES - 1] = 1;
-    }
-    else if(memcmp(cpy_ip, dhcp->subnet, SUBNET_BYTES) == (1 << (TOTAL_BITS - dhcp->mask)) - 1)
-    {
-        cpy_ip[SUBNET_BYTES - 1] = 253;
-    }
-    else if(memcmp(cpy_ip, dhcp->subnet, SUBNET_BYTES) == (1 << (TOTAL_BITS - dhcp->mask)) - 2)
-    {
-        cpy_ip[SUBNET_BYTES - 1] = 253;
-    }
+    unsigned char smallest_ip[SUBNET_BYTES];
 
-    for (i = 0; i < SUBNET_BYTES; ++i) 
-    {
-        for (bit = 0; bit < BITS_IN_BYTE; ++bit) 
-        {
-            bit_val = (cpy_ip[i] >> (BITS_IN_BYTE - 1 - bit)) & 1;
-            if (current->children[bit_val] == NULL) 
-            {
-                current->children[bit_val] = CreateNode(current);
-                if (current->children[bit_val] == NULL) 
-                    return 0;
-            }
-            current = current->children[bit_val];
+    memcpy(cpy_ip, ip, SUBNET_BYTES);
+
+    if (IsReservedIp(cpy_ip, dhcp->subnet, dhcp->mask)) {
+        if (FindNextClosestAvailableIp(dhcp->root, ip, dest_ip, dhcp->mask)) {
+            return 1;
+        } else {
+            return 0; 
         }
     }
 
-    if (current->is_full) 
-    {
-        smallest = FindSmallest(dhcp->root);
-        if (smallest == NULL) return 0;
-
-        GetIpFromNode(smallest,current_ip, dhcp->mask);
-        memcpy(dest_ip, current_ip, SUBNET_BYTES);
+    for (i = 0; i < SUBNET_BYTES; ++i) {
+        for (bit = 0; bit < BITS_IN_BYTE; ++bit) {
+            bit_val = (cpy_ip[i] >> (BITS_IN_BYTE - 1 - bit)) & 1;
+            if (current->children[bit_val] == NULL) {
+                current->children[bit_val] = CreateNode(current);
+                if (current->children[bit_val] == NULL) return 0;
+            current = current->children[bit_val];  
+            }
+            else if(current->is_full)
+            {
+                current = FindSmallest2(current->parent);
+            }
+        }
     }
-    else 
-    {
+
+        GetIpFromNode(current, dest_ip, dhcp->mask);
+
         current->is_full = 1;
         memcpy(dest_ip, cpy_ip, SUBNET_BYTES);
-    }
 
-    return 1; 
-}
-
-int FreeIp(dhcp_t *dhcp, unsigned char ip[SUBNET_BYTES])
-{
-    node_t *current = dhcp->root;
-    size_t i =0 , bit = 0;
-    int bit_val = 0;
-
-    for(i = 0; i<SUBNET_BYTES; ++i)
-    {
-        for(bit = 0; bit < BITS_IN_BYTE; ++bit)
-        {
-            bit_val = (ip[i] >> (BITS_IN_BYTE -1 - bit)) & 1;
-            if(current->children[bit_val] == NULL) return 0;
-            
-            current =  current->children[bit_val];
-        }
-    }
-    if(0 == current->is_full) return 0;
-
-    current->is_full = 0;
     return 1;
 }
 
 
 
-/*Helpers*/
-static node_t *CreateNode(node_t *parent)
-{
-    node_t *new_node = malloc(sizeof(node_t));
-    if(NULL == new_node) return NULL;
+
+int FreeIp(dhcp_t *dhcp, unsigned char ip[SUBNET_BYTES]) {
+    node_t *current;
+    size_t i, bit;
+    int bit_val;
+
+    current = dhcp->root;
+
+    for (i = 0; i < SUBNET_BYTES; ++i) {
+        for (bit = 0; bit < BITS_IN_BYTE; ++bit) {
+            bit_val = (ip[i] >> (BITS_IN_BYTE - 1 - bit)) & 1;
+            if (current->children[bit_val] == NULL) return 0;
+
+            current = current->children[bit_val];
+        }
+    }
+
+    if (0 == current->is_full) return 0;
+
+    current->is_full = 0;
+    return 1;
+}
+
+/* Helpers */
+static node_t *CreateNode(node_t *parent) {
+    node_t *new_node;
+
+    new_node = (node_t *)malloc(sizeof(node_t));
+    if (NULL == new_node) return NULL;
 
     new_node->is_full = 0;
     new_node->parent = parent;
@@ -176,8 +154,7 @@ static node_t *CreateNode(node_t *parent)
     return new_node;
 }
 
-static void FreeNode(node_t *node)
-{
+static void FreeNode(node_t *node) {
     if (node == NULL) return;
 
     FreeNode(node->children[LEFT]);
@@ -186,48 +163,33 @@ static void FreeNode(node_t *node)
     free(node);
 }
 
-static size_t CountFreeNodes(node_t *node)
-{
-    size_t count = 0;
+static size_t CountFreeNodes(node_t *node) {
+    size_t count;
+
     if (NULL == node) return 0;
 
-    count = 0 == node->is_full ? 1 : 0;
+    count = (0 == node->is_full) ? 1 : 0;
 
     count += CountFreeNodes(node->children[LEFT]);
     count += CountFreeNodes(node->children[RIGHT]);
+    
     return count;
 }
 
-static node_t *FindSmallest(node_t *node)
-{
-    node_t *left = NULL;
 
-    if(node == NULL) return NULL;
-    
-    left = FindSmallest(node->children[LEFT]);
-    
-    if(left != NULL && left->is_full != 1) return left;
-
-    return FindSmallest(node->children[RIGHT]);
-}
-
-
-static void GetIpFromNode(node_t *node, unsigned char *ip, size_t mask)
-{
+static void GetIpFromNode(node_t *node, unsigned char *ip, size_t mask) {
     unsigned char current_ip[SUBNET_BYTES] = {0};
-    node_t *current = NULL;
-    size_t position = 0;
-    size_t byte = 0;
-    size_t bit = 0;
+    node_t *current;
+    size_t position;
+    size_t byte;
+    size_t bit;
 
     current = node;
+    position = 0;
 
-    while (current != NULL && current->parent != NULL)
-    {
-        if (position < TOTAL_BITS - mask)
-        {
-            if (current == current->parent->children[RIGHT])
-            {
+    while (current != NULL && current->parent != NULL) {
+        if (position < TOTAL_BITS - mask) {
+            if (current == current->parent->children[RIGHT]) {
                 byte = position / BITS_IN_BYTE;
                 bit = position % BITS_IN_BYTE;
                 current_ip[byte] |= (1 << (BITS_IN_BYTE - 1 - bit));
@@ -238,4 +200,82 @@ static void GetIpFromNode(node_t *node, unsigned char *ip, size_t mask)
     }
 
     memcpy(ip, current_ip, SUBNET_BYTES);
+}
+
+static int IsReservedIp(const unsigned char *ip, const unsigned char *subnet, size_t mask) {
+    unsigned char reserved_start[SUBNET_BYTES];
+    unsigned char reserved_end[SUBNET_BYTES];
+    unsigned char server_ip[SUBNET_BYTES];
+
+    memcpy(reserved_start, subnet, SUBNET_BYTES);
+    memcpy(reserved_end, subnet, SUBNET_BYTES);
+    memcpy(server_ip, subnet, SUBNET_BYTES);
+
+    reserved_start[SUBNET_BYTES - 1] = 0;
+    reserved_end[SUBNET_BYTES - 1] = OCTET_MAX;
+    server_ip[SUBNET_BYTES - 1] = OCTET_MAX - 1;
+
+    if (memcmp(ip, reserved_start, SUBNET_BYTES) == 0 ||
+        memcmp(ip, reserved_end, SUBNET_BYTES) == 0 ||
+        memcmp(ip, server_ip, SUBNET_BYTES) == 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+
+static int FindSmallestAvailableIp(node_t *node, unsigned char *smallest_ip, size_t mask) {
+    if (node == NULL) return 0;
+
+    if (0 == node->is_full) {
+        GetIpFromNode(node, smallest_ip, mask);
+        return 1;
+    }
+
+    if (FindSmallestAvailableIp(node->children[LEFT], smallest_ip, mask)) return 1;
+    if (FindSmallestAvailableIp(node->children[RIGHT], smallest_ip, mask)) return 1;
+
+}
+
+
+static int FindNextClosestAvailableIp(node_t *node, const unsigned char *ip, unsigned char *next_ip, size_t mask) {
+    unsigned char current_ip[SUBNET_BYTES];
+    int found = 0;
+
+    if (node == NULL) return 0;
+
+    if (!node->is_full) {
+        GetIpFromNode(node, current_ip, mask);
+        if (memcmp(current_ip, ip, SUBNET_BYTES) > 0) {
+            memcpy(next_ip, current_ip, SUBNET_BYTES);
+            return 1;
+        }
+    }
+
+    if (FindNextClosestAvailableIp(node->children[LEFT], ip, next_ip, mask)) return 1;
+    if (FindNextClosestAvailableIp(node->children[RIGHT], ip, next_ip, mask)) return 1;
+
+    return found;
+}
+
+
+static node_t *FindSmallest2(node_t *parent)
+{
+    while(parent != NULL)
+    {
+        if(parent->children[LEFT]->is_full && parent->children[RIGHT]->is_full)
+        {
+            parent->is_full = 1;
+            parent = parent->parent;
+        }
+        if(parent->children[RIGHT] == NULL)
+        {
+            parent->children[RIGHT] = CreateNode(parent);
+            return parent->children[RIGHT];
+            
+        }
+        if(parent->children[RIGHT]->is_full = 0)
+            return parent->children[RIGHT];
+    }
 }
--- ds/test/dhcp_test.c
+++ ds/test/dhcp_test.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+
 #include "dhcp.h"
+
 
 enum children {
     LEFT = 0,
     RIGHT = 1,
     NUMBER_OF_CHILD = 2
-};
-
-struct dhcp {
-    node_t *root;
-    size_t mask;
-    unsigned char subnet[SUBNET_BYTES];
-    
 };
 
 struct node {
@@ -22,29 +18,29 @@ struct node {
     node_t *children[NUMBER_OF_CHILD];
 };
 
+struct dhcp {
+    node_t *root;
+    size_t mask;
+    unsigned char subnet[SUBNET_BYTES];
+};
+
 void test_DHCPCreateDestroy() {
     unsigned char subnet[SUBNET_BYTES] = {192, 168, 1, 0};
     size_t mask = 24;
 
     dhcp_t *dhcp = DHCPCreate(subnet, mask);
     if (dhcp != NULL) {
-
         printf("DHCPCreate Success\n");
         printf("Subnet: %d.%d.%d.%d\n", dhcp->subnet[0], dhcp->subnet[1], dhcp->subnet[2], dhcp->subnet[3]);
         printf("Mask: %lu\n", dhcp->mask);
-
         DHCPDestroy(dhcp);
-
         printf("DHCPDestroy Success\n\n");
     } else {
-
         printf("DHCPCreate Failed\n");
     }
 }
 
 void test_CountFreeIps() {
-
-
     size_t mask = 24;
     unsigned char subnet[SUBNET_BYTES] = {192, 168, 1, 0};
     unsigned char ip[SUBNET_BYTES] = {192, 168, 1, 1};
@@ -58,18 +54,15 @@ void test_CountFreeIps() {
     unsigned char dest_broadcast[SUBNET_BYTES];
     unsigned char dest_server[SUBNET_BYTES];
 
-    size_t free_ips = 0;
     dhcp_t *dhcp = DHCPCreate(subnet, mask);
     if (dhcp != NULL) {
         size_t free_ips = CountFreeIps(dhcp);
 
-        if(free_ips != 253)
-        {
-             printf("FAIL !!! CountFreeIps Before Allocation: %lu\n", free_ips);
-        }
-        else
+        if (free_ips != 253) {
+            printf("FAIL !!! CountFreeIps Before Allocation: %lu\n", free_ips);
+        } else {
             printf("Free IP's before allocation Success.\n");
-       
+        }
 
         if (AllocateIp(dhcp, ip, dest_ip)) {
             printf("Allocated IP: %d.%d.%d.%d\n", dest_ip[0], dest_ip[1], dest_ip[2], dest_ip[3]);
@@ -77,60 +70,58 @@ void test_CountFreeIps() {
             printf("FAIL !!! CountFreeIps AllocateIp failed.\n");
         }
 
-        if (AllocateIp(dhcp, broadcast, dest_broadcast))
-        {
+        AllocateIp(dhcp, broadcast, dest_broadcast);
+        if (memcmp(broadcast, dest_broadcast, SUBNET_BYTES) == 0) {
             printf("FAIL !!!- allocated Broadcast IP.\n");
             printf("Allocated IP: %d.%d.%d.%d\n", dest_broadcast[0], dest_broadcast[1], dest_broadcast[2], dest_broadcast[3]);
         }
-        
-        if (AllocateIp(dhcp, network, dest_network))
-        {
+
+        AllocateIp(dhcp, network, dest_network);
+        if (memcmp(network, dest_network, SUBNET_BYTES) == 0) {
             printf("FAIL !!!- allocated Network IP.\n");
             printf("Allocated IP: %d.%d.%d.%d\n", dest_network[0], dest_network[1], dest_network[2], dest_network[3]);
         }
-        if (AllocateIp(dhcp, server, dest_server))
-        {
-            printf("FAIL !!!- allocated Network IP.\n");
+        AllocateIp(dhcp, server, dest_server);
+        if (memcmp(server, dest_server, SUBNET_BYTES) == 0) {
+            printf("FAIL !!!- allocated Server IP.\n");
             printf("Allocated IP: %d.%d.%d.%d\n", dest_server[0], dest_server[1], dest_server[2], dest_server[3]);
         }
 
-        /**/
-        if(AllocateIp(dhcp, ip2, dest_ip2))
-        {
+        if (AllocateIp(dhcp, ip2, dest_ip2)) {
             printf("2nd Allocation: %d.%d.%d.%d\n", dest_ip2[0], dest_ip2[1], dest_ip2[2], dest_ip2[3]);
         }
-        if(AllocateIp(dhcp, ip2, dest_ip))
-        {
-            printf("FAIL !! allocated same address\n");
-            
-        }
-        else
-        {
+            FreeIp(dhcp, dest_ip2);
+        if (AllocateIp(dhcp, ip2, dest_ip)) {
+            printf("After free 2nd Allocation: %d.%d.%d.%d\n", dest_ip[0], dest_ip[1], dest_ip[2], dest_ip[3]);
+        } else {
             printf("Double allocation Success\n");
-            printf("2nd Allocation: %d.%d.%d.%d\n", dest_ip[0], dest_ip[1], dest_ip[2], dest_ip[3]);
+            printf("2nd Allocation AFTER: %d.%d.%d.%d\n", dest_ip[0], dest_ip[1], dest_ip[2], dest_ip[3]);
+            printf("2nd Allocation BEFORE: %d.%d.%d.%d\n", dest_ip2[0], dest_ip2[1], dest_ip2[2], dest_ip2[3]);
         }
-            
-
-
 
         free_ips = CountFreeIps(dhcp);
- 
-        if(free_ips != 251) 
-        {
-            printf("FAIL !!!   CountFreeIps after allocation: %lu\n", free_ips);
+        if (free_ips != 251) {
+            printf("FAIL !!! CountFreeIps after allocation: %lu\n", free_ips);
+        } else {
+            printf("Free Ips Success for %lu mask\n.", mask);
         }
-        else
-            printf("Free Ips Success for %lu mask\n.",mask);
-        
 
+        FreeIp(dhcp, dest_ip2);
+        FreeIp(dhcp, dest_ip);
+
+        free_ips = CountFreeIps(dhcp);
+        if (free_ips != 253) {
+            printf("FAIL !!! CountFreeIps after FREEING: %lu\n\n", free_ips);
+        } else {
+            printf("Free Ips Success for %lu mask\n\n.", mask);
+        }
         DHCPDestroy(dhcp);
     } else {
-        printf("DHCPCreate Failed\n");
+        printf("DHCPCreate Failed\n\n");
     }
 }
 
 void test_AllocateIp() {
-
     unsigned char subnet[SUBNET_BYTES] = {192, 168, 1, 0};
     size_t mask = 24;
 
@@ -138,30 +129,35 @@ void test_AllocateIp() {
     if (dhcp != NULL) {
         unsigned char ip[SUBNET_BYTES] = {192, 168, 1, 1};
         unsigned char dest_ip[SUBNET_BYTES];
-                unsigned char new_ip[SUBNET_BYTES] = {192, 168, 1, 2};
+        unsigned char new_ip[SUBNET_BYTES] = {192, 168, 1, 2};
         unsigned char new_dest_ip[SUBNET_BYTES];
+        unsigned char new_ip2[SUBNET_BYTES] = {192, 168, 1, 2};
+        unsigned char new_dest_ip2[SUBNET_BYTES];
 
         if (AllocateIp(dhcp, ip, dest_ip)) {
-
             printf("AllocateIp Success\n");
             printf("Allocated IP: %d.%d.%d.%d\n", dest_ip[0], dest_ip[1], dest_ip[2], dest_ip[3]);
         } else {
-
             printf("AllocateIp Failed\n");
         }
 
         if (AllocateIp(dhcp, new_ip, new_dest_ip)) {
-
             printf("AllocateIp (new IP) Success\n");
             printf("Allocated IP: %d.%d.%d.%d\n", new_dest_ip[0], new_dest_ip[1], new_dest_ip[2], new_dest_ip[3]);
         } else {
-
             printf("AllocateIp (new IP) Failed\n");
+        }
+
+
+        if (AllocateIp(dhcp, new_ip2, new_dest_ip2)) {
+            printf("Allocated DOUBLE ip check Success\n");
+            printf("Allocated IP: %d.%d.%d.%d\n\n\n", new_dest_ip2[0], new_dest_ip2[1], new_dest_ip2[2], new_dest_ip2[3]);
+        } else {
+            printf("AllocateIp DOUBLE ip check Failed - DID NOT ALLOCATE\n");
         }
 
         DHCPDestroy(dhcp);
     } else {
-
         printf("DHCPCreate Failed\n");
     }
 }
@@ -169,47 +165,38 @@ void test_AllocateIp() {
 void test_FreeIp() {
     unsigned char subnet[SUBNET_BYTES] = {192, 168, 1, 0};
     size_t mask = 24;
-            unsigned char ip[SUBNET_BYTES] = {192, 168, 1, 1};
-        unsigned char dest_ip[SUBNET_BYTES];
+    unsigned char ip[SUBNET_BYTES] = {192, 168, 1, 1};
+    unsigned char dest_ip[SUBNET_BYTES];
 
     dhcp_t *dhcp = DHCPCreate(subnet, mask);
     if (dhcp != NULL) {
-
         if (AllocateIp(dhcp, ip, dest_ip)) {
             printf("Allocated IP: %d.%d.%d.%d\n", dest_ip[0], dest_ip[1], dest_ip[2], dest_ip[3]);
         } else {
             printf("AllocateIp failed\n");
         }
 
-
         if (FreeIp(dhcp, ip)) {
             printf("FreeIp Success\n");
         } else {
-
             printf("FreeIp Failed\n");
         }
         if (!FreeIp(dhcp, ip)) {
-
             printf("FreeIp (second attempt) Success\n");
         } else {
-
             printf("FreeIp (second attempt) Failed\n");
         }
 
         DHCPDestroy(dhcp);
     } else {
-
         printf("DHCPCreate Failed\n");
     }
 }
 
 int main() {
-
     test_CountFreeIps();
-  /*
     test_AllocateIp();
     test_FreeIp();
     test_DHCPCreateDestroy();
-    */
     return 0;
 }
