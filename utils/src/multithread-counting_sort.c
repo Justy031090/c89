7ecaa732113de88e79a719fa1baba66f3574850b
Michael.Bar <justy031090@gmail.com>
2024-09-25 18:20:51 +0300

    tested

--- utils/src/multithread-counting_sort.c
+++ utils/src/multithread-counting_sort.c
@@ -1,35 +1,53 @@
 #include <pthread.h> /*thread operations*/
 #include <stddef.h> /*size_t*/
-#include <stdlib.h> /*Calloc*/
+#include <stdlib.h> /*malloc*/
+#include <ctype.h> /*tolower*/
+#include <string.h> /*strlen*/
+#include <stdio.h> /*printf*/
+#include <time.h> /*clock*/
 #include <assert.h>
 
+#define NUM_OF_LETTER 26
+#define NUM_OF_THREADS 2
+#define MAX_STRINGS 150
+#define MAX_STRING_LENGTH 30
+#define PATH_TO_DICT "/usr/share/dict/words"
 
-    /*Each thread should recieve a portion of the unsored array*/
-    /*Each thread should perform a count sort on this portion*/
-    /*Main thread recieves the sorted mini arrays*/
 typedef struct 
 {
-    int *arr;
+    char **arr;
     size_t start;
     size_t end;
+    size_t max_len;
 } thread_data_t;
 
-
-static int MaxArrayValue(int *numbers, size_t size);
 void *CountingSort(void *data);
 
-int *MultiThreadSort(int *unsorted, size_t size, size_t num_of_threads)
+char **MultiThreadSort(char **unsorted, size_t size, size_t num_of_threads)
 {
-    pthread_t threads[num_of_threads];
-    size_t i = 0, start = 0;
-    size_t portion = size / num_of_threads;
-    size_t remainder = size % num_of_threads;
-    thread_data_t thread_data[num_of_threads];
-    for(;i<num_of_threads;++i)
+    pthread_t threads[NUM_OF_THREADS];
+    size_t i, j, start = 0, max = 0;
+    size_t portion = size / NUM_OF_THREADS;
+    size_t remainder = size % NUM_OF_THREADS;
+    thread_data_t thread_data[NUM_OF_THREADS];
+
+    (void)num_of_threads;
+
+    /* Convert all strings to lowercase */
+    for(i = 0; i < size; ++i)
+    {
+        for(j = 0; j < strlen(unsorted[i]); ++j)
+        {
+            unsorted[i][j] = tolower(unsorted[i][j]);
+        }
+    }
+
+    for(i = 0; i < NUM_OF_THREADS; ++i)
     {
         thread_data[i].arr = unsorted;
         thread_data[i].start = start;
         thread_data[i].end = start + portion;
+        thread_data[i].max_len = max;
 
         if(remainder > i)
         {
@@ -39,69 +57,117 @@ int *MultiThreadSort(int *unsorted, size_t size, size_t num_of_threads)
         start = thread_data[i].end;
 
         pthread_create(&threads[i], NULL, CountingSort, &thread_data[i]);
-    };
+    }
 
-    for(i = 0; i< num_of_threads; ++i)
+    for(i = 0; i < NUM_OF_THREADS; ++i)
     {
-        pthread_join(threads[i],NULL);
+        pthread_join(threads[i], NULL);
     }
 
     return unsorted;
 }
 
-
-
-
 void *CountingSort(void *data)
 {
-	int max = 0;
-	int *count = NULL, *numbers = NULL;
-	size_t i = 0, j = 0, start, end, size;
+    int count[NUM_OF_LETTER] = {0};
+    char **words = NULL;
+    char **output = NULL;
+    char c;
+    size_t position, i, start, end, size, max;
     thread_data_t *thread_data = NULL;
 
-	assert(data);
-    size = end - start;
+    assert(data);
     thread_data = (thread_data_t *)data;
-    numbers = thread_data->arr;
+    words = thread_data->arr;
     start = thread_data->start;
     end = thread_data->end;
+    size = end - start;
+    max = thread_data->max_len;
 
-	max = MaxArrayValue(numbers[start],size );
-    count = calloc(max +1, sizeof(int));
+    output = (char **)malloc(size * sizeof(char *));
+    if(!output) return NULL;
 
-	if(NULL == count)
+    for(position = max - 1; position < max; --position)
     {
-        pthread_exit(NULL);
+        for(i = start; i < end; ++i)
+        {
+            c = strlen(words[i]) > position ? words[i][position] : 0;
+            count[c - 'a' + 1]++;
+        }
+        for(i = 1; i <= NUM_OF_LETTER; ++i)
+        {
+            count[i] += count[i - 1];
+        }
+
+        for(i = end - 1; i >= start; --i)
+        {
+            c = strlen(words[i]) > position ? words[i][position] : 0;
+            output[--count[c - 'a' + 1]] = words[i];
+        }
+
+        for(i = start; i < end; ++i)
+        {
+            words[i] = output[i - start];
+        }
     }
 
-    for(i = start, i< end; ++i)
-    {
-        count[numbers[i]]++;
-    }
-	
-	for(i = 0; (int)i<=max; ++i)
-	{
-		while(count[i]>0)
-
-		{
-			numbers[j++] = (int)i;
-			count[i]--;
-		}
-	}
-	free(count);
-	pthread_exit(NULL);
+    free(output);
+    pthread_exit(NULL);
 }
 
-static int MaxArrayValue(int *numbers, size_t size)
+void PrintArray(char **arr, size_t size)
 {
-    int i = 0, max = numbers[0];
-    while(i<size)
+    size_t i;
+    for(i = 0; i < size; ++i)
     {
-        if(numbers[i] > max)
-        {
-            max = numbers[i];
-        }
-        ++i;
+        printf("%s\n", arr[i]);
     }
-    return max;
+}
+
+int main()
+{
+    FILE *dictionary = fopen(PATH_TO_DICT, "r");
+    char *arr[MAX_STRINGS];
+    char buffer[MAX_STRING_LENGTH];
+    size_t i = 0, j = 0;
+    clock_t start, stop;
+
+    if(dictionary == NULL)
+    {
+        printf("failed to open dictionary\n");
+        return 0;
+    }
+
+
+    while(i < MAX_STRINGS && fgets(buffer, sizeof(buffer), dictionary) != NULL)
+    {
+        
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        arr[i] = (char *)malloc((strlen(buffer) + 1) * sizeof(char));
+        strcpy(arr[i], buffer);
+        i++;
+    }
+
+    fclose(dictionary);
+
+
+    printf("Original Array:\n");
+    PrintArray(arr, i);
+
+    start = clock();
+    MultiThreadSort(arr, i, NUM_OF_THREADS);
+    stop = clock();
+
+    printf("\nSorted Array:\n");
+    PrintArray(arr, i);
+
+    printf("\nTime taken to sort: %ld seconds\n", (stop - start)/CLOCKS_PER_SEC);
+    for (j = 0; j < i; ++j)
+    {
+        free(arr[j]);
+    }
+
+
+    return 0;
 }
